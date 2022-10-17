658b980a43aa25d5bce76f12935e46dfb22f6861
심경선/클라우드BIZ팀/223595 <223595@ssg.global>
2022-10-17 20:14:29 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Join from './pages/Join';
+import Create from './pages/Create';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+    <Route path="/" element={<Main />} />
+    <Route path="/join" element={<Join />} />
+    <Route path="/create" element={<Create />} />
+    <Route path="/mypage" element={<MyPage />} />
+  </Routes>
   );
 }
 
