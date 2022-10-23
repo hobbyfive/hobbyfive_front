2a9305ddcb7951a84eaf17cec52604796bc53ad0
A <A@aa.com>
2022-10-23 18:39:14 +0900

    ClubDetail titleArea, part of options

--- src/components/ClubDetail.jsx
+++ src/components/ClubDetail.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import './ClubDetail.css';
+
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 const style = {
   position: 'absolute',
@@ -31,7 +34,40 @@ export default function ClubDetail() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}></Box>
+        <Box sx={style}>
+          <div className="dTitleArea">
+            <div className="dClubTitle">모임제목(dummy)</div>
+            <div className="timeArea">
+              <div className="dGatheringDay">
+                <Stack direction="row" spacing={1}>
+                  <Chip label="모임 예정일" />
+                  <Chip
+                    label="2022년 11월 4일 18시(dummy)"
+                    variant="outlined"
+                  />
+                </Stack>
+              </div>
+              <div className="dDeadLine">
+                <Stack direction="row" spacing={1}>
+                  <Chip label="모집 마감일" />
+                  <Chip
+                    label="2022년 11월 3일 18시(dummy)"
+                    variant="outlined"
+                  />
+                </Stack>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="dOptions">
+            <div className="dLeftOptions">
+              <div className="dRegion">Region</div>
+              <div className="dCategory">Category</div>
+            </div>
+            <div className="dParticipants"></div>
+          </div>
+          <hr />
+        </Box>
       </Modal>
     </div>
   );
