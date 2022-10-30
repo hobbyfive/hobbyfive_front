e2186eea712217c3ef3cc974f2562c4731569a33
Gyeongseon Sim <sseon@Gyeongseonui-MacBookAir.local>
2022-10-30 20:15:59 +0900

    refact: 로그인 모달 구현

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,88 +1,38 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Login.css";
+import React, {useState} from 'react';
+import '/node_modules/bootstrap/dist/css/bootstrap.css';
+import '../pages/SignUp.css'
 
-class Login extends Component {
-  state = {
-    email: "",
-    password: "",
-  };
-
-  loginHandler = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };   ////계산된 속성명 사용
-
-//   loginClickHandler = () => {
-//     const { email, password } = this.state;
-//     fetch("/login", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         email,
-//         password,
-//       }),
-//     })
-//       .then((res) => res.json())
-//       .then((res) => console.log(res));
-//   }; 
-
-  render() {
-    const { isOpen, close } = this.props;   //아까 버튼에서 props로 가져온것
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
     return (
-      <>
-        {isOpen ? (  
-
-         ////만약 isopen(this.state.isModalOpen)이 true일때 코드를 실행 false면  null
-        /// <div onClick={close}> 로그인창 말고 회색 바탕을 누를시 모달이 꺼지게 만듬
-	      ///<span className="close" onClick={close}>&times;</span> x버튼 누를시 꺼짐
-        ////<div className="modalContents" onClick={isOpen}> 로그인 화면은 버튼 클릭해서 들어오면
-         /// true인 상태로 있어서 화면이 안꺼진다.
-      
-          <div className="modal">
-            <div onClick={close}>
-              <div className="loginModal">
-                <span className="close" onClick={close}>
-                  &times;
-                </span>
-                <div className="modalContents" onClick={isOpen}>
-                  <img
-                    className="signinIcon"
-                    src="/Images/SignIn/signinIcon.png"
-                  />
-                  <input
-                    name="email"
-                    className="loginId"
-                    type="text"
-                    placeholder="아이디"
-                    onChange={this.loginHandler}
-                  />
-                  <input
-                    name="password"
-                    className="loginPw"
-                    type="password"
-                    placeholder="비밀번호"
-                    onChange={this.loginHandler}
-                  />
-                  <button className="loginBtn" onClick={this.loginClickHandler}>
-                    {" "}
-                    로그인{" "}
-                  </button>
-                  <div className="loginEnd">
-                    <div className="loginLine">
-                      회원이 아니신가요? <Link to="/signup">회원가입</Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
+        <div className="modals custommodal">
+        <div className="modals-dialog" role="document">
+            <div className="modals-content">
+            <div className="modals-header">
+                <h5 className="modals-title">로그인 하기</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modals" aria-label="Close">
+                <span aria-hidden="true"></span>
+                </button>
             </div>
-          </div>
-        ) : null}
-      </>
+            <div className="modals-body">
+            <div className="form-floating mb-3"><input name="email" type="email" placeholder="name@example.com" value={email} onChange={onEmailHandler} className="form-control"/><label htmlFor="floatingInput">Email address</label></div>
+            <div className="form-floating mb-3"><input name="password" type="password" placeholder="Password" value={password} onChange={onPasswordHandler} className="form-control"/><label htmlFor="floatingInput">Password</label></div>
+            </div>
+            <div className="modals-footer">
+                <button type="button" className="btn btn-primary">Login</button>
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modals">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
     );
-  }
-}
+};
 
-export default Login;
+export { Login };
--- src/pages/ModalTemp.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Login from '../components/Login'
-
-export default function ModalTemp() {
-    return (
-        <div><Login></Login></div>
-    )
-}
