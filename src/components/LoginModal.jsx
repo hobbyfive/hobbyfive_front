e2b904887bf2d57b71231c8aeac53cdb255e50a7
심경선/클라우드BIZ팀/223595 <223595@ssg.global>
2022-11-02 20:31:13 +0900

    refact: signup 모달로 수정

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -12,19 +12,21 @@ const LoginModal = ({closeLoginModal}) => {
               <div className="modal-header">
                 <h5 className="modal-title">Login</h5>
               </div>
+              <div className="input_box_login">
               <div className="modal-body">
               <div className="form-group">
-              <label htmlFor="exampleInputEmail1" className="form-label mt-4">Email address</label>
+              <label htmlFor="exampleInputEmail1" className="form-label mt-4 left_box">Email 주소</label>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
             </div>
             <div className="form-group mb-4">
-              <label htmlFor="exampleInputPassword1" className="form-label mt-4">Password</label>
+              <label htmlFor="exampleInputPassword1" className="form-label mt-4 left_box">비밀번호</label>
               <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
               </div>
+              </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-primary" onClick={requestLogin}>Login</button>
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeLoginModal}>Close</button>
+                <button type="button" className="btn btn-primary" onClick={requestLogin}>로그인</button>
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeLoginModal}>나중에</button>
               </div>
             </div>
           </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { useNavigate } from "react-router-dom";
-import './SignUp.css'
+import '../components/SignUp.css'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -47,23 +47,29 @@ function SignUp() {
   }
 
   return (
-    <div className="loginregister">
-      <form>
-          <div className="form-floating mb-3"><input name="email" type="email" placeholder="name@example.com" value={email} onChange={onEmailHandler} className="form-control"/><label for="floatingInput">Email address</label></div>
-          <div className="form-floating mb-3"><input name="password" type="password" placeholder="Password" value={password} onChange={onPasswordHandler} className="form-control"/><label for="floatingInput">Password</label></div>
-          <div className="form-floating mb-3"><input name="confirmPassword" type="password" placeholder="ConfirmPassword" value={confirmPassword} onChange={onConfirmPasswordHandler} className="form-control"/><label for="floatingInput">Confirm Password</label></div>
-          <div className="form-floating mb-3"><input name="name" type="text" placeholder="Name" value={name} onChange={onNameHandler} className="form-control"/><label for="floatingInput">Name</label></div>
-          <div className="form-floating mb-3"><input name="nickname" type="text" placeholder="NickName" value={nickname} onChange={onNickNameHandler} className="form-control"/><label for="floatingInput">Nickname</label></div>
-          <div className="form-floating mb-3"><input name="company" type="text" placeholder="Company" value={company} onChange={onCompanyHandler} className="form-control"/><label for="floatingInput">Company</label></div>
-          <div className="form-check">
-            <input id="gender_man" type="radio" name="gender" className="form-check-input"/><label className="form-check-label" for="gender_man">
-              I am a man.</label></div>
-          <div className="form-check">
-            <input id="gender_woman" type="radio" name="gender" className="form-check-input"/><label className="form-check-label" for="gender_man">
-              I am a woman.</label>
-          </div>
-          <div className="submit-button"><button type="submit" onClick={() => onSubmit()} className="btn btn-primary">I'm ready to create my account!</button></div>
-      </form>
+
+    <div>
+      <div className='center_box'>
+            <h1>HOBBYFIVE</h1>
+      </div>
+      <div className="loginregister">
+        <form>
+            <div className="form-floating mb-3 input_box"><input name="email" type="email" placeholder="name@example.com" value={email} onChange={onEmailHandler} className="form-control"/><label for="floatingInput">Email address</label></div>
+            <div className="form-floating mb-3"><input name="password" type="password" placeholder="Password" value={password} onChange={onPasswordHandler} className="form-control"/><label for="floatingInput">Password</label></div>
+            <div className="form-floating mb-3"><input name="confirmPassword" type="password" placeholder="ConfirmPassword" value={confirmPassword} onChange={onConfirmPasswordHandler} className="form-control"/><label for="floatingInput">Confirm Password</label></div>
+            <div className="form-floating mb-3"><input name="name" type="text" placeholder="Name" value={name} onChange={onNameHandler} className="form-control"/><label for="floatingInput">Name</label></div>
+            <div className="form-floating mb-3"><input name="nickname" type="text" placeholder="NickName" value={nickname} onChange={onNickNameHandler} className="form-control"/><label for="floatingInput">Nickname</label></div>
+            <div className="form-floating mb-3"><input name="company" type="text" placeholder="Company" value={company} onChange={onCompanyHandler} className="form-control"/><label for="floatingInput">Company</label></div>
+            <div className="form-check">
+              <input id="gender_man" type="radio" name="gender" className="form-check-input"/><label className="form-check-label" for="gender_man">
+                I am a man.</label></div>
+            <div className="form-check">
+              <input id="gender_woman" type="radio" name="gender" className="form-check-input"/><label className="form-check-label" for="gender_man">
+                I am a woman.</label>
+            </div>
+            <div className="submit-button"><button type="submit" onClick={() => onSubmit()} className="btn btn-primary">I'm ready to create my account!</button></div>
+        </form>
+      </div>
     </div>
   );
 }
