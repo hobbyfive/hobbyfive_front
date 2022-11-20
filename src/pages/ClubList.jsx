1170997ca995c84e3d1734bed50c18ede6462c5a
A <A@aa.com>
2022-11-20 14:13:13 +0900

    2022-11-20

--- src/pages/ClubList.jsx
+++ src/pages/ClubList.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import ClubDetail from '../components/ClubDetail';
 
-import './ClubList.css'
+import './ClubList.css';
 import FilterModal from './FilterModal';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const ClubList = ({selectMenu, userId}) => {
+const ClubList = ({ selectMenu, userId }) => {
   const [clubList, setClubList] = useState([]);
   const [filterVisible, setFilterVisible] = useState(false);
   const [clubVisible, setClubVisible] = useState(false);
@@ -14,122 +14,169 @@ const ClubList = ({selectMenu, userId}) => {
 
   const closeFilterModal = () => {
     setFilterVisible(!filterVisible);
-  }
+  };
 
-  const closeClubModal = (clubId) => {
+  const closeClubModal = clubId => {
     setClubVisible(!clubVisible);
     setSwitchId(clubId);
-  }
+  };
 
-  
-  
-  const AllClub = (selectMenu) => {
-    console.log(selectMenu); 
-    console.log("전체선택함");
-    useEffect(() => {
-      axios.get("http://34.236.154.248:8090/api/club/allClub")
-        .then((res) => {
-          console.log(res.data);
-          setClubList(res.data);
-        })
-        .catch((error) => {
-          throw new Error(error);
-        });
-    }, [clubList]);
-    
-  }
-
+  const AllClub = selectMenu => {
+    console.log(selectMenu);
+    console.log('전체선택함');
+    axios
+      .get('http://34.236.154.248:8090/api/club/allClub')
+      .then(res => {
+        console.log(res.data);
+        setClubList(res.data);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  };
 
   const CompanyClub = (selectMenu, userId) => {
-    console.log("회사선택함");
+    console.log('회사선택함');
     console.log(selectMenu);
-    useEffect(() => {
-      axios.get(`http://34.236.154.248:8090/api/club/selectClubByCompanyId/${userId}`)
-        .then((res) => {
-          setClubList(res.data);
-          console.log(res.data);
-        })
-        .catch((error) => {
-          throw new Error(error);
-        });
-    }, [clubList]);
-    
-  }
+    axios
+      .get(
+        `http://34.236.154.248:8090/api/club/selectClubByCompanyId/${userId}`,
+      )
+      .then(res => {
+        setClubList(res.data);
+        console.log(res.data);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
+  };
 
   if (selectMenu == 0) {
     AllClub(selectMenu);
   } else {
     CompanyClub(selectMenu, userId);
   }
-  
 
-    return (
-        <div>
+  return (
+    <div>
+      <div className="createClub">
+        <Link to="/create" className="a-tag">
+          모임 개설
+        </Link>
+      </div>
 
-          <div className='createClub'>
-            <Link to="/create" className='a-tag'>모임 개설</Link>
+      <div className="clublistGrid">
+        <div className="filter">
+          {filterVisible && <FilterModal closeFilterModal={closeFilterModal} />}
+          <div
+            className="breadcrumb btn btn-outline-secondary bg"
+            onClick={closeFilterModal}
+          >
+            FILTER
           </div>
-
-          <div className='clublistGrid'>
-
-            <div className='filter'>
-              {filterVisible && <FilterModal closeFilterModal={closeFilterModal} />}
-              <div className='breadcrumb btn btn-outline-secondary bg' onClick={closeFilterModal}>FILTER</div>
-              <div className='filter_title'>Date</div>
-              <div className='filter_content'>전체</div>
-              <div>------------------</div>
-              <div className='filter_title'>Category</div>
-              <div className='filter_content'>전체</div>
-              <div>------------------</div>
-              <div className='filter_title'>Location</div>
-              <div className='filter_content'>전체</div>
-            </div>
-            
-            <div className='clubs'>
-                {clubVisible && <ClubDetail closeClubModal={closeClubModal} clubId={switchId}/> }
-               
-               
-                {
-                  
-                  clubList.map((club, index) => (
-                    
-                    <div className="card custom-card" key={index} onClick={() => closeClubModal(club.clubId)}>
-                      
-                      <div className="custom-card-body">
-                        <img className='custom-card-img' alt="HTML" src={club.imageUrl}/>
-                        <span className="badge bg-info">{club.status != null ? club.status.description : "없음"}</span>
-                        <span className="badge bg-secondary">{club.categoryId.name}</span>
-                        <h4 className="custom-card-title">{club.name}</h4>
-                        <p className="card-text">신청 인원 : {club.currNum}/{club.maxNum} ({club.minNum}명이상 개설)</p>
-                        <p className="card-text">모임 예정일 : {club.meetTime.slice(0, 4)}/{club.meetTime.slice(5, 7)}/{club.meetTime.slice(8,10)} {club.meetTime.slice(11, 13)}:{club.meetTime.slice(14, 16)}</p>
-                        <p className="card-text">모집 마감일 : {club.expiryTime.slice(0, 4)}/{club.expiryTime.slice(5, 7)}/{club.expiryTime.slice(8,10)} {club.expiryTime.slice(11, 13)}:{club.expiryTime.slice(14, 16)}</p>
-                      </div>
-                    </div>
-                  ))
-                }
-                
-
-              
-              <div className='page-wrap'>
-                <ul className="pagination pagination-sm">
-                  <li className="page-item"><a className="page-link" href="#none">&#60;</a></li>
-                  <li className="page-item active"><a className="page-link" href="#none">1</a></li>
-                  <li className="page-item"><a className="page-link" href="#none">2</a></li>
-                  <li className="page-item"><a className="page-link" href="#none">3</a></li>
-                  <li className="page-item"><a className="page-link" href="#none">4</a></li>
-                  <li className="page-item"><a className="page-link" href="#none">5</a></li>
-                  <li className="page-item"><a className="page-link" href="#none">&#62;</a></li>
-                </ul>
-              </div>
-
-            </div>
-            
-          </div>
-
-          
+          <div className="filter_title">Date</div>
+          <div className="filter_content">전체</div>
+          <div>------------------</div>
+          <div className="filter_title">Category</div>
+          <div className="filter_content">전체</div>
+          <div>------------------</div>
+          <div className="filter_title">Location</div>
+          <div className="filter_content">전체</div>
         </div>
 
-    );
+        <div className="clubs">
+          {clubVisible && (
+            <ClubDetail closeClubModal={closeClubModal} clubId={switchId} />
+          )}
+
+          {clubList.map((club, index) => (
+            <div
+              className="card custom-card"
+              key={index}
+              onClick={() => closeClubModal(club.clubId)}
+            >
+              <div className="custom-card-body">
+                <img
+                  className="custom-card-img"
+                  alt="HTML"
+                  src={club.imageUrl}
+                />
+                {club.status.description == '모집중' ? (
+                  <span className="badge bg-success">
+                    {club.status.description}
+                  </span>
+                ) : (
+                  <span className="badge bg-danger">
+                    {club.status.description}
+                  </span>
+                )}
+                <span className="badge bg-secondary">
+                  {club.categoryId.name}
+                </span>
+                <h4 className="custom-card-title">{club.name}</h4>
+                <p className="card-text">지역 : {club.districtId.name}</p>
+                <p className="card-text">
+                  신청 인원 : {club.currNum}/{club.maxNum} ({club.minNum}명이상
+                  개설)
+                </p>
+                <p className="card-text">
+                  모임 예정일 : {club.meetTime.slice(0, 4)}/
+                  {club.meetTime.slice(5, 7)}/{club.meetTime.slice(8, 10)}{' '}
+                  {club.meetTime.slice(11, 13)}:{club.meetTime.slice(14, 16)}
+                </p>
+                <p className="card-text">
+                  모집 마감일 : {club.expiryTime.slice(0, 4)}/
+                  {club.expiryTime.slice(5, 7)}/{club.expiryTime.slice(8, 10)}{' '}
+                  {club.expiryTime.slice(11, 13)}:
+                  {club.expiryTime.slice(14, 16)}
+                </p>
+              </div>
+            </div>
+          ))}
+
+          <div className="page-wrap">
+            <ul className="pagination pagination-sm">
+              <li className="page-item">
+                <a className="page-link" href="#none">
+                  &#60;
+                </a>
+              </li>
+              <li className="page-item active">
+                <a className="page-link" href="#none">
+                  1
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#none">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#none">
+                  3
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#none">
+                  4
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#none">
+                  5
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#none">
+                  &#62;
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ClubList;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,102 +2,121 @@ import React, { useEffect, useState } from 'react';
 import { LoginModal } from '../components/LoginModal';
 import ClubList from './ClubList';
 import { SignUpModal } from '../components/SignUpModal';
-import hobbyfiveloggo from './high-five.png'
+import hobbyfiveloggo from './high-five.png';
 
-import './Main.css'
+import './Main.css';
 import axios from 'axios';
 
 const Main = () => {
-
-  const[menu, setMenu] = useState(0);
-  const[loginVisible, setLoginVisible] = useState(false);
-  const[SignUpVisible, setSignUpVisible] = useState(false);
-  const[loginStatus, setLoginStatus] = useState(false);
-  const[signupStatus, setSignupStatus] = useState(false);
-  const[companyName, setCompanyName] = useState('');
-  const[userId, setUserId] = useState(0);
+  const [menu, setMenu] = useState(0);
+  const [loginVisible, setLoginVisible] = useState(false);
+  const [SignUpVisible, setSignUpVisible] = useState(false);
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [signupStatus, setSignupStatus] = useState(false);
+  const [companyName, setCompanyName] = useState('');
+  const [userId, setUserId] = useState(0);
 
   const closeLoginModal = () => {
     setLoginVisible(!loginVisible);
-  }
+  };
 
   const closeSignUpModal = () => {
     setSignUpVisible(!SignUpVisible);
-  }
+  };
 
-  const changeMenu = (menuIndex) =>{
+  const changeMenu = menuIndex => {
     setMenu(menuIndex);
-  }
-  
-  const getLoginStatus = (loginStatus) => {
-    setLoginStatus(loginStatus);
-  }
+  };
 
-  const getSignUpStatus = (signupStatus) => {
+  const getLoginStatus = loginStatus => {
+    setLoginStatus(loginStatus);
+  };
+
+  const getSignUpStatus = signupStatus => {
     setSignupStatus(signupStatus);
-  }
+  };
 
   if (loginStatus) {
     console.log('loginSuccess');
     console.log(localStorage.getItem('JWT'));
-    axios( `http://34.236.154.248:8090/api/user`,
-    {
+    axios(`http://34.236.154.248:8090/api/user`, {
       headers: { Authorization: `Bearer ${localStorage.getItem('JWT')}` },
     })
-    .then(res => {
-      setUserId(res.data.userId);
-      setCompanyName(res.data.companyId.name);
-      console.log(res.data);
-    })
-    .catch((error) => {
-      throw new Error(error);
-    });
-    
+      .then(res => {
+        setUserId(res.data.userId);
+        setCompanyName(res.data.companyId.name);
+        console.log(res.data);
+      })
+      .catch(error => {
+        throw new Error(error);
+      });
   }
-  
-    return (
-      <div>
-        {/* 타이틀 */}
-        <div className='center_box'>
-        <img src={hobbyfiveloggo} alt='logo' className='img'/>
-          <h1>HOBBYFIVE</h1>
-        </div>
 
-        {/* 로그인, 회원가입 */}
-        <div className='right_box'>
-        {loginVisible && <LoginModal closeLoginModal={closeLoginModal} getLoginStatus={getLoginStatus} />}
-        {SignUpVisible && <SignUpModal closeSignUpModal={closeSignUpModal} getSignUpStatus={getSignUpStatus} />}
-        {loginStatus ?
-            <div>
-            <a className='a-tag' >로그아웃</a>
-            <a> / </a>
-            <a className='a-tag' >마이페이지</a> </div>
-            : 
-            <div>
-            <a className='a-tag' onClick={closeLoginModal}>로그인</a>
-            <a> / </a>
-            <a className='a-tag' onClick={closeSignUpModal}>회원가입</a> </div>}
-          
-          
-        </div>
-
-        {/* 메인 탭 */}
-        <div className="wrap">
-          <div className="menuBar">
-            <ul className="tabs">
-              <li className={`${menu === 0? 'active': 'inactive'}`} onClick={() => changeMenu(0)}>전체</li>
-              <li className={`${menu === 1? 'active': 'inactive'}`} onClick={() => changeMenu(1)}>{companyName}</li>
-            </ul>
-          </div>
-          <div className="contents">
-             <ClubList selectMenu={menu} userId={userId} /> 
-          </div>
-        </div>
-
-
+  return (
+    <div>
+      {/* 타이틀 */}
+      <div className="center_box">
+        <img src={hobbyfiveloggo} alt="logo" className="img" />
+        <h1>HOBBYFIVE</h1>
       </div>
 
-    );
+      {/* 로그인, 회원가입 */}
+      <div className="right_box">
+        {loginVisible && (
+          <LoginModal
+            closeLoginModal={closeLoginModal}
+            getLoginStatus={getLoginStatus}
+          />
+        )}
+        {SignUpVisible && (
+          <SignUpModal
+            closeSignUpModal={closeSignUpModal}
+            getSignUpStatus={getSignUpStatus}
+          />
+        )}
+        {loginStatus ? (
+          <div>
+            <a className="a-tag">로그아웃</a>
+            <a> / </a>
+            <a className="a-tag">마이페이지</a>{' '}
+          </div>
+        ) : (
+          <div>
+            <a className="a-tag" onClick={closeLoginModal}>
+              로그인
+            </a>
+            <a> / </a>
+            <a className="a-tag" onClick={closeSignUpModal}>
+              회원가입
+            </a>{' '}
+          </div>
+        )}
+      </div>
+
+      {/* 메인 탭 */}
+      <div className="wrap">
+        <div className="menuBar">
+          <ul className="tabs">
+            <li
+              className={`${menu === 0 ? 'active' : 'inactive'}`}
+              onClick={() => changeMenu(0)}
+            >
+              전체
+            </li>
+            <li
+              className={`${menu === 1 ? 'active' : 'inactive'}`}
+              onClick={() => changeMenu(1)}
+            >
+              {companyName}
+            </li>
+          </ul>
+        </div>
+        <div className="contents">
+          <ClubList selectMenu={menu} userId={userId} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
